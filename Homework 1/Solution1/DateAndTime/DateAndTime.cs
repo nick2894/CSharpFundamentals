a2d72fcedfebdde8c8ffb8c340a34ae43dd13024
nick2894 <milushev_nikolay@yahoo.bg>
2016-10-22 19:53:20 +0300

    Homework

---
+++ Homework 1/Solution1/DateAndTime/DateAndTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class DateAndTime
+    {
+        static void Main()
+        {
+            Console.WriteLine("Today is");
+            Console.WriteLine(DateTime.Now);
+        }
+
+            
+    }
+}
---
+++ Homework 1/Solution1/Number12345/number12345.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Number12345
+{
+    class number12345
+    {
+        static void Main()
+        {
+            int number = 12345;
+            Console.WriteLine(Math.Sqrt(number));
+
+        }
+    }
+}
